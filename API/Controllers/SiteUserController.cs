8fc0e3bfee48275bada45f5e24c205058bc8a06e
Seemorgh-113 <Seemorgh-113@SEEMORGH-113>
2019-07-02 15:46:48 +0430

    Site user API Added

---
+++ API/Controllers/SiteUserController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace API.Controllers
+{
+    public class SiteUserController : ApiController
+    {
+        private SHS2Entities db = new SHS2Entities();
+        // GET: api/SiteUser
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/SiteUser/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/SiteUser
+        public IHttpActionResult Post([FromBody]Users siteUser)
+        {
+            var luser = new Users();
+            var luser1 = from a in db.Users
+                         where a.Name == siteUser.Name && a.Pass == siteUser.Pass && a.Active == true
+                         select a;
+            var myuser = luser1.ToList();
+            luser.Role1 = myuser.ElementAt(0).Role1;
+            return Ok(luser);
+        }
+
+        // PUT: api/SiteUser/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/SiteUser/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
