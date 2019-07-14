98cc7a0d1fa70147acee57a9850b9ae1737dc3fd
Mandana <Seemorgh-129@SEEMORGH-129>
2019-07-14 13:46:11 +0430

    DB coonection is eddited

--- API/Controllers/PersonCTRController.cs
+++ API/Controllers/PersonCTRController.cs
@@ -5,6 +5,9 @@ using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
+using System.Web.Http.Results;
+using System.Web.Mvc;
+using HttpPostAttribute = System.Web.Mvc.HttpPostAttribute;
 
 namespace API.Controllers
 {
@@ -12,16 +15,20 @@ namespace API.Controllers
     {
 
         private SHS2Entities db = new SHS2Entities();
+
+
+
+
         // GET: api/PersonCTR
         public IEnumerable<Person> Get()
         {
             var luser1 = from a in db.Person
                          select a;
             var myuser = luser1.ToList();
-
-
             return myuser;
         }
+
+
 
         // GET: api/PersonCTR/5
         public string Get(int id)
@@ -34,22 +41,62 @@ namespace API.Controllers
 
         [HttpPost]
         // POST: api/SiteUser
-        public IHttpActionResult Post([FromBody]Person test)
+        public IHttpActionResult Post([FromBody]Person siteUser)
         {
 
-            Person a = new Person();
-            a.Name = test.Name;
-            a.NationalId = test.NationalId;
-            a.Age = test.PersonalCode;
-            a.PersonalCode = test.PersonalCode;
-            a.PhoneNumber = test.PhoneNumber;
-            a.UserName = test.UserName;
-            a.Password = test.Password;
 
-            db.Person.Add(a);
-            db.SaveChangesAsync();
-            return Ok(test);
+
+            // /////////////////////
+            var luser = new Person();
+            var luser1 = from b in db.Person
+                         where b.UserName == siteUser.UserName
+                         select b;
+            //           var myuser = luser1.ToList();
+            //           return Ok(luser);
+
+
+
+            if (luser1.ToList().Count == 0)
+            {
+
+
+                Person a = new Person();
+                a.Name = siteUser.Name;
+                a.NationalId = siteUser.NationalId;
+                var person1 = from b in db.Person
+                              where b.Name == siteUser.Name && b.NationalId == siteUser.NationalId
+                              select b;
+                if (person1.ToList().Count == 0)
+                {
+                    a.Age = siteUser.PersonalCode;
+                    a.PersonalCode = siteUser.PersonalCode;
+                    a.PhoneNumber = siteUser.PhoneNumber;
+                    a.UserName = siteUser.UserName;
+                    a.Password = siteUser.Password;
+
+
+                    db.Person.Add(a);
+                    db.SaveChangesAsync();
+                    return Ok(siteUser);
+
+
+                }
+                else
+                {
+                    luser.NationalId = "Registered";
+                    return Ok(luser);
+                }
+            }
+            else
+            {
+                luser.Name = "Repeated";
+                return Ok(luser);
+            }
+
+
         }
+
+
 
 
 
@@ -65,3 +112,4 @@ namespace API.Controllers
         }
     }
 }
+
